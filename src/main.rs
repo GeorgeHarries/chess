08902b3a533775ba39d8eabb71e9a56b8763481e
George Harries <george.harries@gmail.com>
2023-07-17 10:08:08 +0100

    Added piece selection logic

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::str::FromStr;
-
 // bevy namespace uses
 use bevy::prelude::*;
 
@@ -42,15 +40,14 @@ pub enum Kind {
 #[derive(Component)]
 pub struct Square {
     file: u8,
-    rank: u8,
-    selected: bool
+    rank: u8
 }
 
 #[derive(Component, Debug)]
 pub struct Piece {
     team: Team,
     kind: Kind,
-    position: (u8, u8)
+    coords: (u8, u8)
 }
 
 ////////////////////////////////////////////////////////////////
@@ -75,7 +72,7 @@ fn main() {
         spawn_board,
         spawn_pieces
     ))
-    .add_systems(Update, select_square)
+    .add_systems(Update, select_piece)
     .run();
 }
 
@@ -115,7 +112,7 @@ fn spawn_board(
                 ..default()
             })
             .insert(Name::new(format!("{}{}", file_to_char(file), rank_to_char(rank))))
-            .insert(Square {rank, file, selected: false});
+            .insert(Square {rank, file});
         }
     }
 }
@@ -126,7 +123,7 @@ fn spawn_pieces(
 ) {
     let pieces: Vec<Piece> = fen_string_to_piece_vec(STARTING_FEN_STRING).unwrap();
     for piece in pieces {
-        let world_position = board_to_world(piece.position);
+        let world_position = board_to_world(piece.coords);
         let piece_name = get_piece_name(&piece.team, &piece.kind);
         commands.spawn(SpriteBundle {
             sprite: Sprite {
@@ -144,60 +141,32 @@ fn spawn_pieces(
     }
 }
 
-fn select_square(
+fn select_piece(
     buttons: Res<Input<MouseButton>>,
     window: Query<&Window>,
     camera: Query<(&Camera, &GlobalTransform)>,
-    mut squares: Query<(&mut Square, &mut Sprite)>
+    squares: Query<(&Square, &mut Sprite)>,
+    pieces: Query<&Piece>
 ) {
-    if buttons.just_pressed(MouseButton::Left) {
-        let (camera, camera_transform) = camera.single();
-        if let Some(position) = window
-            .single()
-            .cursor_position()
-            .and_then(|cursor| camera
-                .viewport_to_world(camera_transform, cursor)
-            )
-            .map(|ray| ray.origin.truncate()) 
-        {
-            if position.x >= BOARD_CENTRE.x - 0.5*BOARD_SIZE &&
-               position.x <= BOARD_CENTRE.x + 0.5*BOARD_SIZE &&
-               position.y >= BOARD_CENTRE.y - 0.5*BOARD_SIZE &&
-               position.y <= BOARD_CENTRE.y + 0.5*BOARD_SIZE
-            {
-                let (file, rank) = world_to_board(position);
-                for (mut square, mut sprite) in squares.iter_mut() {
-                    if (square.rank + square.file) % 2 == 0 {
-                        if square.file == file && square.rank == rank {
-                            sprite.color = BOARD_SQUARE_WHITE_SELECT_COLOUR;
-                            square.selected = true;
-                        } else {
-                            sprite.color = BOARD_SQUARE_WHITE_COLOUR;
-                            square.selected = false;
-                        }
-                    } else {
-                        if square.file == file && square.rank == rank {
-                            sprite.color = BOARD_SQUARE_BLACK_SELECT_COLOUR;
-                            square.selected = true;
-                        } else {
-                            sprite.color = BOARD_SQUARE_BLACK_COLOUR;
-                            square.selected = false;
-                        }
-                    }
-                }
-            } else {
-                for (square, mut sprite) in squares.iter_mut() {
-                    if (square.rank + square.file) % 2 == 0 {
-                        sprite.color = BOARD_SQUARE_WHITE_COLOUR;
-                    } else {
-                        sprite.color = BOARD_SQUARE_BLACK_COLOUR;
-                    }
-                }
-            }
+    if !buttons.just_pressed(MouseButton::Left) {return}
+
+    let (camera, camera_transform) = camera.single();
+    let position = window.single().cursor_position().and_then(|cursor|camera.viewport_to_world(camera_transform, cursor)).map(|ray| ray.origin.truncate());
+    if position.is_none() {return}
+    
+    let position = position.unwrap();
+    if !is_position_on_board(&position) {clear_square_selections(squares); return}
+
+    let selected_coords: (u8, u8) = world_to_board(position);
+    for piece in pieces.iter() {
+        if piece.coords == selected_coords {
+            colour_selected_square(squares, selected_coords);
+            return;
         }
     }
-}
 
+    clear_square_selections(squares);
+}
 
 ////////////////////////////////////////////////////////////////
 // Helper functions
@@ -231,9 +200,9 @@ fn rank_to_char(rank: u8) -> char {
     }
 }
 
-fn world_to_board(coords: Vec2) -> (u8, u8) {
-    let file = ((coords.x - BOARD_CENTRE.x + 0.5*BOARD_SIZE) / SQUARE_SIZE).ceil() as u8;
-    let rank = ((coords.y - BOARD_CENTRE.y + 0.5*BOARD_SIZE) / SQUARE_SIZE).ceil() as u8;
+fn world_to_board(position: Vec2) -> (u8, u8) {
+    let file = ((position.x - BOARD_CENTRE.x + 0.5*BOARD_SIZE) / SQUARE_SIZE).ceil() as u8;
+    let rank = ((position.y - BOARD_CENTRE.y + 0.5*BOARD_SIZE) / SQUARE_SIZE).ceil() as u8;
     return (file, rank);
 }
 
@@ -242,6 +211,13 @@ fn board_to_world(coords: (u8, u8)) -> Vec2 {
     let x: f32 = BOARD_CENTRE.x - (4.5 - file as f32) * SQUARE_SIZE;
     let y: f32 = BOARD_CENTRE.y - (4.5 - rank as f32) * SQUARE_SIZE;
     return Vec2::new(x, y);
+}
+
+fn is_position_on_board(&position: &Vec2) -> bool {
+    position.x >= BOARD_CENTRE.x - 0.5*BOARD_SIZE &&
+    position.x <= BOARD_CENTRE.x + 0.5*BOARD_SIZE &&
+    position.y >= BOARD_CENTRE.y - 0.5*BOARD_SIZE &&
+    position.y <= BOARD_CENTRE.y + 0.5*BOARD_SIZE
 }
 
 fn get_piece_name(team: &Team, kind: &Kind) -> String {
@@ -264,23 +240,23 @@ fn get_piece_name(team: &Team, kind: &Kind) -> String {
 
 fn fen_string_to_piece_vec(fen: &str) -> Result<Vec<Piece>, String> {
     let mut piece_vec: Vec<Piece> = Vec::new();
-    let mut current_rank: u8 = 8;
-    let mut current_file: u8 = 1;
+    let mut current_rank: u8 = 8;  // TODO: Improve this with assertions they are in the range [1:8]
+    let mut current_file: u8 = 1;  // TODO: Improve this with assertions they are in the range [1:8]
 
     for char in fen.chars() {
         match char {
-            'P'       => {piece_vec.push(Piece {team: Team::WHITE, kind: Kind::PAWN,   position: (current_file, current_rank)}); current_file += 1},
-            'N'       => {piece_vec.push(Piece {team: Team::WHITE, kind: Kind::KNIGHT, position: (current_file, current_rank)}); current_file += 1},
-            'B'       => {piece_vec.push(Piece {team: Team::WHITE, kind: Kind::BISHOP, position: (current_file, current_rank)}); current_file += 1},
-            'R'       => {piece_vec.push(Piece {team: Team::WHITE, kind: Kind::ROOK,   position: (current_file, current_rank)}); current_file += 1},
-            'Q'       => {piece_vec.push(Piece {team: Team::WHITE, kind: Kind::QUEEN,  position: (current_file, current_rank)}); current_file += 1},
-            'K'       => {piece_vec.push(Piece {team: Team::WHITE, kind: Kind::KING,   position: (current_file, current_rank)}); current_file += 1},
-            'p'       => {piece_vec.push(Piece {team: Team::BLACK, kind: Kind::PAWN,   position: (current_file, current_rank)}); current_file += 1},
-            'n'       => {piece_vec.push(Piece {team: Team::BLACK, kind: Kind::KNIGHT, position: (current_file, current_rank)}); current_file += 1},
-            'b'       => {piece_vec.push(Piece {team: Team::BLACK, kind: Kind::BISHOP, position: (current_file, current_rank)}); current_file += 1},
-            'r'       => {piece_vec.push(Piece {team: Team::BLACK, kind: Kind::ROOK,   position: (current_file, current_rank)}); current_file += 1},
-            'q'       => {piece_vec.push(Piece {team: Team::BLACK, kind: Kind::QUEEN,  position: (current_file, current_rank)}); current_file += 1},
-            'k'       => {piece_vec.push(Piece {team: Team::BLACK, kind: Kind::KING,   position: (current_file, current_rank)}); current_file += 1},
+            'P'       => {piece_vec.push(Piece {team: Team::WHITE, kind: Kind::PAWN,   coords: (current_file, current_rank)}); current_file += 1},
+            'N'       => {piece_vec.push(Piece {team: Team::WHITE, kind: Kind::KNIGHT, coords: (current_file, current_rank)}); current_file += 1},
+            'B'       => {piece_vec.push(Piece {team: Team::WHITE, kind: Kind::BISHOP, coords: (current_file, current_rank)}); current_file += 1},
+            'R'       => {piece_vec.push(Piece {team: Team::WHITE, kind: Kind::ROOK,   coords: (current_file, current_rank)}); current_file += 1},
+            'Q'       => {piece_vec.push(Piece {team: Team::WHITE, kind: Kind::QUEEN,  coords: (current_file, current_rank)}); current_file += 1},
+            'K'       => {piece_vec.push(Piece {team: Team::WHITE, kind: Kind::KING,   coords: (current_file, current_rank)}); current_file += 1},
+            'p'       => {piece_vec.push(Piece {team: Team::BLACK, kind: Kind::PAWN,   coords: (current_file, current_rank)}); current_file += 1},
+            'n'       => {piece_vec.push(Piece {team: Team::BLACK, kind: Kind::KNIGHT, coords: (current_file, current_rank)}); current_file += 1},
+            'b'       => {piece_vec.push(Piece {team: Team::BLACK, kind: Kind::BISHOP, coords: (current_file, current_rank)}); current_file += 1},
+            'r'       => {piece_vec.push(Piece {team: Team::BLACK, kind: Kind::ROOK,   coords: (current_file, current_rank)}); current_file += 1},
+            'q'       => {piece_vec.push(Piece {team: Team::BLACK, kind: Kind::QUEEN,  coords: (current_file, current_rank)}); current_file += 1},
+            'k'       => {piece_vec.push(Piece {team: Team::BLACK, kind: Kind::KING,   coords: (current_file, current_rank)}); current_file += 1},
             '/'       => {current_file = 1; current_rank -= 1},
             '1'..='8' => {current_file += char.to_digit(10).unwrap() as u8},
             _         => return Result::Err(format!("Non-fen char '{}' found in fen string", char))  // TODO: Improve this with proper error type
@@ -288,4 +264,32 @@ fn fen_string_to_piece_vec(fen: &str) -> Result<Vec<Piece>, String> {
     }
 
     return Result::Ok(piece_vec);
+}
+
+fn clear_square_selections(mut squares: Query<(&Square, &mut Sprite)>) {
+    for (square, mut sprite) in squares.iter_mut() {
+        if (square.rank + square.file) % 2 == 0 {
+            sprite.color = BOARD_SQUARE_WHITE_COLOUR;
+        } else {
+            sprite.color = BOARD_SQUARE_BLACK_COLOUR;
+        }
+    }
+}
+
+fn colour_selected_square(mut squares: Query<(&Square, &mut Sprite)>, selected_coords: (u8, u8)) {
+    for (square, mut sprite) in squares.iter_mut() {
+        if (square.rank + square.file) % 2 == 0 {
+            if (square.file, square.rank) == selected_coords {
+                sprite.color = BOARD_SQUARE_WHITE_SELECT_COLOUR;
+            } else {
+                sprite.color = BOARD_SQUARE_WHITE_COLOUR;
+            }
+        } else {
+            if (square.file, square.rank) == selected_coords {
+                sprite.color = BOARD_SQUARE_BLACK_SELECT_COLOUR;
+            } else {
+                sprite.color = BOARD_SQUARE_BLACK_COLOUR;
+            }
+        }
+    }
 }
